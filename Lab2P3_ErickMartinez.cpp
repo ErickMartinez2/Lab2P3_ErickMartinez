8fd568aa79d2088fe68c224ee49f7d072d36b59a
ErickMartinez2 <erickmartinez2203@gmail.com>
2017-07-28 13:21:28 -0600

    Menu Creado

---
+++ Lab2P3_ErickMartinez.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int opcion;
+	do {
+		cout << "   Menu" << endl
+			 << "1. Calcular Distancia" << endl
+			 << "2. Calcular Seno y Coseno" << endl
+			 << "3. Calcular Triangulo" << endl
+			 << "4. Salir" << endl
+			 << "Ingrese la opción: ";
+		cin >> opcion;
+		switch (opcion) {
+			case 1:
+				
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				cout << "Hasta Pronto!" << endl;
+				break;
+		}
+	} while (opcion != 4);
+	return 0;
+}
